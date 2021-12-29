55002875a86dc9e7968dd364371d6a6a1dc533fd
gmaksaam <gmaksaam@gmail.com>
2021-12-29 22:23:34 +0000

    floral: Use kernel headers from stock kernel

--- device.mk
+++ device.mk
@@ -101,6 +101,9 @@ $(call inherit-product, $(LOCAL_PATH)/utils.mk)
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
+# Kernel headers
+PRODUCT_VENDOR_KERNEL_HEADERS := device/google/coral/sm8150/kernel-headers
+
 PRODUCT_CHARACTERISTICS := nosdcard
 PRODUCT_SHIPPING_API_LEVEL := 29
 
